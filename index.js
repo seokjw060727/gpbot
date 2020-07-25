8a970a689d5b6072086eb70f21d3c5e39d73422c
bot <seokjw0727@naver.com>
2020-07-25 14:25:17 +0900

    add bot state

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
   console.log('봇 켜짐.');
   client.user.setPresence({ game: { name: '게임페인서버 온라인!' }, status: 'online' })
 });
- 
+
 client.on('message', (message) => {
   if(message.author.bot) return;
 
@@ -37,6 +37,71 @@ function checkPermission(message) {
   } else {
     return false;
   }
+}
+
+if(message.content.startsWith('!청소')) {
+  if(checkPermission(message)) return
+
+  var clearLine = message.content.slice('!청소 '.length);
+  var isNum = !isNaN(clearLine)
+
+  if(isNum && (clearLine <= 0 || 100 < clearLine)) {
+    message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+    return;
+  } else if(!isNum) { // c @나긋해 3
+    if(message.content.split('<@').length == 2) {
+      if(isNaN(message.content.split(' ')[2])) return;
+
+      var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
+      var count = parseInt(message.content.split(' ')[2])+1;
+      const _limit = 10;
+      let _cnt = 0;
+
+      message.channel.fetchMessages({limit: _limit}).then(collected => {
+        collected.every(msg => {
+          if(msg.author.id == user) {
+            msg.delete();
+            ++_cnt;
+          }
+          return !(_cnt == count);
+        });
+      });
+    }
+  } else {
+    message.channel.bulkDelete(parseInt(clearLine)+1)
+      .then(() => {
+        AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
+      })
+      .catch(console.error)
+  }
+}
+
+function checkPermission(message) {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+  message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+  return true;
+} else {
+  return false;
+}
+}
+
+function changeCommandStringLength(str, limitLen = 8) {
+let tmp = str;
+limitLen -= tmp.length;
+
+for(let i=0;i<limitLen;i++) {
+    tmp += ' ';
+}
+
+return tmp;
+}
+
+async function AutoMsgDelete(message, str, delay = 3000) {
+let msg = await message.channel.send(str);
+
+setTimeout(() => {
+  msg.delete();
+}, delay);
 }
 
 client.login(token);
